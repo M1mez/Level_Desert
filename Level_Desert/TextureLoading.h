5e8cabaaf4c555b504f86830a77ed64b6e2e1c70
Johannes Fessler <if16b042@technikum-wien.at>
2018-12-18 21:06:25 +0100

    changed testureparameter to GL_LINEAR_MIPMAP_LINEAR

--- Level_Desert/TextureLoading.h
+++ Level_Desert/TextureLoading.h
@@ -59,8 +59,8 @@ inline unsigned int storeTexture(std::string path, bool isTransparent, Shader *s
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded textures on the y-axis.
